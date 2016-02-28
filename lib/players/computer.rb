f7e4dec084e70524ce99e63c400f6f64657a9e9e
aprietof <adrianprieto00@gmail.com>
2016-02-27 19:19:49 -0500

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,6 @@
 class Computer < Player # inherits from Player 
 
-  returns a valid position for the computer to move
+  # returns a valid position for the computer to move
   def move(board)
     move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
   end
